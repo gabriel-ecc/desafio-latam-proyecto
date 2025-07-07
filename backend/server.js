1e1a568c9b74a0c0ce7c11a6115e1b33eb4128c6
gabriel <gabriel.castillo.cardenas@gmail.com>
2025-07-07 18:18:26 -0400

    products: responde ruta categoria

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ import pool from './db/schema/config.js'
 // importamos las rutas
 import registerUser from './routes/userRoutes.js'
 import authRoutes from './routes/authRoutes.js'
+import categoriesRoutes from './routes/categoriesRoutes.js'
+
+
 
 // configuramos el puerto del servidor que va a escuchar
 const PORT = process.env.PORT || 3000
@@ -20,6 +23,7 @@ app.use(express.json())
 // cargamos las rutas
 app.use('/', registerUser)
 app.use('/', authRoutes)
+app.use('/', categoriesRoutes)
 
 // Connect to the database and then start the server
 pool.query('SELECT NOW()', (err, res) => {
