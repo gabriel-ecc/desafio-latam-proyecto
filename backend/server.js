06bad7ec6867062880887f58f6f3d41bb5ed0001
gabriel <gabriel.castillo.cardenas@gmail.com>
2025-07-16 18:38:03 -0400

    update products y constantes para api/v1

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,9 @@ app.use(express.json())
 // cargamos las rutas (/api para funcionar mejor con el frontend, (modificarlo si se estima conveniente)).
 app.use('/api/v1', registerUser)
 app.use('/api/v1', authRoutes)
-app.use('/', categoriesRoutes)
-app.use('/', seasonRoutes)
-app.use('/', productsRoutes)
+app.use('/api/v1', categoriesRoutes)
+app.use('/api/v1', seasonRoutes)
+app.use('/api/v1', productsRoutes)
 
 // Connect to the database and then start the server
 pool.query('SELECT NOW()', (err, res) => {
--- frontend/src/config/constants.js
+++ frontend/src/config/constants.js
@@ -1,9 +1,10 @@
 export const URLBASE = 'http://localhost:3000'
+export const apiVersion = '/api/v1'
 
 export const ENDPOINT = {
-  users: `${URLBASE}/users`,
-  products: `${URLBASE}/products`,
-  seasons: `${URLBASE}/seasons`,
-  categories: `${URLBASE}/categories`,
+  users: `${URLBASE}${apiVersion}/users`,
+  products: `${URLBASE}${apiVersion}/products`,
+  seasons: `${URLBASE}${apiVersion}/seasons`,
+  categories: `${URLBASE}${apiVersion}/categories`,
   
 }
