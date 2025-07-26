ca19b3c27468923a13b550bafa7694f06c23bde2
gabriel <gabriel.castillo.cardenas@gmail.com>
2025-07-26 19:46:14 -0400

    update login evitar exponer tipo de error al cliente

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -4,13 +4,14 @@ import jwt from 'jsonwebtoken'
 import 'dotenv/config'
 
 const loginUser = async (req, res) => {
+  const errorMessage = 'Email o Contraseña incorrecta'
   try {
     const { email, password } = req.body
     const user = await findUserByEmailModel(email)
     const status = user.user_status
 
     if (!user) {
-      return res.status(401).json({ message: 'Email o contraseña incorrecta' })
+      return res.status(401).json({ message: errorMessage })
     }
     if (status === 0) {
       return res.status(403).json({
@@ -20,7 +21,7 @@ const loginUser = async (req, res) => {
 
     const isPasswordValid = bcrypt.compareSync(password, user.password)
     if (!isPasswordValid) {
-      return res.status(401).json({ message: 'No autorizado' })
+      return res.status(401).json({ message: errorMessage })
     }
     const userId = user.id
 
@@ -29,39 +30,9 @@ const loginUser = async (req, res) => {
     })
     res.status(200).json({ token })
   } catch (error) {
-    res.status(500).json({ message: 'Error de token' })
+    res.status(500).json({ message: errorMessage })
     console.error(error)
   }
 }
 
-const getUserData = async (req, res) => {
-  try {
-    const email = req.user //
-    console.log(
-      'authController getUserData: Email recibido de req.user: ',
-      email
-    )
-    const user = await findUserByEmailModel(email)
-
-    if (!user) {
-      console.log(
-        'authController getUserData: Usuario NO encontrado en la bd para el email: ',
-        email
-      )
-      return res.status(404).json({ message: 'Usuario NO encontrado en la bd' })
-    }
-
-    // Devolvemos solo los datos permitidos
-    const userData = {
-      nameLastName: user.nameLastName,
-      email: user.email,
-      rol: user.rol
-    }
-    res.status(200).json(userData)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ message: 'Error al obtener los datos del usuario.' })
-  }
-}
-
-export { loginUser, getUserData }
+export { loginUser }
