622eaf4ddc8f3b6910b9d4e1c63cbfdc7b449be8
gabriel <gabriel.castillo.cardenas@gmail.com>
2025-07-26 02:13:31 -0400

    update config 10 porcent for testing

--- backend/test/controllers/products.spec.js
+++ backend/test/controllers/products.spec.js
@@ -8,4 +8,23 @@ describe('API /products', () => {
       expect(response.statusCode).toBe(200)
     })
   })
+  describe('GET /api/v1/products/frontPage', () => {
+    it('Deberia retornar status 200', async () => {
+      const response = await request(app).get('/api/v1/products/frontPage')
+      expect(response.statusCode).toBe(200)
+    })
+  })
+  describe('GET /api/v1/seasons', () => {
+    it('Deberia retornar status 200', async () => {
+      const response = await request(app).get('/api/v1/seasons')
+      expect(response.statusCode).toBe(200)
+    })
+  })
+  describe('GET /api/v1/categories', () => {
+    it('Deberia retornar status 200', async () => {
+      const response = await request(app).get('/api/v1/categories')
+      expect(response.statusCode).toBe(200)
+    })
+  })
 })
+
