dc17c894c2571556b3005b974c5fd1c9743527f5
gabriel <gabriel.castillo.cardenas@gmail.com>
2025-07-26 10:12:55 -0400

    update test users

--- backend/test/controllers/users.spec.js
+++ backend/test/controllers/users.spec.js
@@ -17,4 +17,66 @@ describe('API /users', () => {
       expect(response.statusCode).toBe(201)
     })
   })
+  describe('Mismo POST /api/v1/users', () => {
+    it('Deberia retornar status 409', async () => {
+      const userData = {
+        firstName: 'test',
+        lastName: 'test',
+        email: 'test@test.com',
+        phone: '123456789',
+        password: '123456',
+        userType: 1
+      }
+
+      const response = await request(app).post('/api/v1/users').send(userData)
+      expect(response.statusCode).toBe(409)
+    })
+  })
+  describe('Incompleto POST /api/v1/users', () => {
+    it('Deberia retornar status 409', async () => {
+      const userData = {
+        firstName: 'test',
+        lastName: null,
+        email: 'test@test.com',
+        phone: '123456789',
+        password: '123456',
+        userType: 1
+      }
+
+      const response = await request(app).post('/api/v1/users').send(userData)
+      expect(response.statusCode).toBe(400)
+    })
+  })
+
+  describe('Password corta POST /api/v1/users', () => {
+    it('Deberia retornar status 409', async () => {
+      const userData = {
+        firstName: 'test',
+        lastName: 'test',
+        email: 'test@test.com',
+        phone: '123456789',
+        password: '1234',
+        userType: 1
+      }
+
+      const response = await request(app).post('/api/v1/users').send(userData)
+      expect(response.statusCode).toBe(400)
+    })
+  })
+
+  describe('Correo no valido POST /api/v1/users', () => {
+    it('Deberia retornar status 409', async () => {
+      const userData = {
+        firstName: 'test',
+        lastName: 'test',
+        email: 'test@test',
+        phone: '123456789',
+        password: '1234',
+        userType: 1
+      }
+
+      const response = await request(app).post('/api/v1/users').send(userData)
+      expect(response.statusCode).toBe(400)
+    })
+  })
 })
