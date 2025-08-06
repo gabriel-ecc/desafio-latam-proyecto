bc7801376d108514ec68d6c98edf27736f1b23c1
Koiristea <koiristeart@gmail.com>
2025-08-06 15:35:44 -0400

    botón de "volver"

--- frontend/src/pages/MyPurchases.jsx
+++ frontend/src/pages/MyPurchases.jsx
@@ -1,7 +1,10 @@
 // Página donde el usuario puede ver el historial de sus compras.
+import BackButton from '../components/BackButton'
+
 export default function MyPurchases() {
   return (
     <div>
+      <BackButton />
       <h2>Mis compras</h2>
       {/* Aquí va el contenido*/}
     </div>
