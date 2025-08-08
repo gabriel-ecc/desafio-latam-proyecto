996388967360510bfc901ad5c205291d6efa9404
Andreina Guerra <141190940+Gealuque@users.noreply.github.com>
2025-08-07 20:27:08 -0400

    Solo falta el pagar

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -6,18 +6,19 @@ import { useProfile } from '../hooks/useProfile.js'
 import axios from 'axios'
 import Button from 'react-bootstrap/Button'
 import '../pages/Cart.css'
-import BackButton from '../components/BackButton'
+import Swal from 'sweetalert2'
 
 const Cart = () => {
   const { cart, setCart, updateQuantity, subtraction, totalPrice, addToCart } = useContext(CartContext)
   const [selectedPayment, setSelectedPayment] = useState(null)
+  const [paymentStep, setPaymentStep] = useState(false)
   const { user } = useProfile()
   const navigate = useNavigate()
 
   const paymentMethods = [
-    { id: 'visa', label: 'Visa', img: '/imgs/visa.png' },
-    { id: 'mastercard', label: 'MasterCard', img: '/imgs/mastercard.png' },
-    { id: 'paypal', label: 'PayPal', img: '/imgs/paypal.png' }
+    { id: 'credito', label: 'Crédito', img: '/imgs/credit_card.png' },
+    { id: 'debito', label: 'Débito', img: '/imgs/debit_card.png' },
+    { id: 'efectivo', label: 'Efectivo', img: '/imgs/cash_payment.png' }
   ]
 
     useEffect(() => {
@@ -26,6 +27,71 @@ const Cart = () => {
       document.body.classList.remove('home-background')
     }
   }, [])
+
+  const handleCancel = () => {
+    Swal.fire({
+    title: '¿Seguro que quieres vaciar el carrito?',
+    text: 'Esta acción no se puede deshacer.',
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Vaciar carrito',
+    cancelButtonText: 'Volver',
+    confirmButtonColor: '#d33',
+    cancelButtonColor: '#3085d6',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      setCart([])  // Vaciar carrito
+      Swal.fire({
+        title: 'Carrito vacío',
+        icon: 'success',
+        confirmButtonText: 'OK',
+        confirmButtonColor: '#28a745',
+      })
+    }
+  })
+
+  }
+
+  const handleContinue = () => {
+    if(cart.length === 0){
+      Swal.fire({
+        title: '¡El Carrito está vacío!',
+        icon: 'warning',
+        confirmButtonText: 'Continuar',
+        confirmButtonColor: '#28a745'
+      })
+    return
+    }
+    setPaymentStep(true)
+  }
+
+  const handleBack = () => {
+    setPaymentStep(false)
+    setSelectedPayment(null)
+  }
+
+  /* En construcción
+  const handlePay = () => {
+    if(!selectedPayment) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Selecciona un método de pago',
+    })
+    return
+    }
+    Swal.fire({
+      title: 'Pago realizado con éxito',
+      html: `
+        <p>Gracias, <strong>${user.name}</strong>!</p>
+        <p>Se ha procesado tu pago correctamente.</p>
+        <p>Correo: ${user.email}</p>`,
+      icon: 'success',
+      confirmButtonText: 'Cerrar'
+    }).then(() => {
+    setCart([])
+    navigate('/')
+  })
+  }*/
 
 // Página del carrito de compras, donde el usuario puede ver y editar los productos que va a comprar.
 
@@ -63,35 +129,68 @@ const Cart = () => {
       <div className='total_option'>
         <h5 className='total_title'>Total: ${totalPrice().toLocaleString('es-CL')} </h5>
         <div className='total_button'>
-        <Button variant='danger'>Cancelar</Button>
-        <Button variant='success'>Continuar</Button>
+        <Button variant='danger' onClick={handleCancel} disabled={paymentStep}>Vaciar Carrito</Button>
+        <Button variant='success' onClick={handleContinue} disabled={paymentStep || cart.length === 0}>Continuar</Button>
       </div>
       </div>
   </div>
-
 
     <div className='section_right'>
       <div className='payment_container'>
         <h2>Verificación y Pago:</h2>
         <p>Total de artículos: {cart.reduce((sum, item) => sum + item.quantity, 0)}</p>
         <p>Total a pagar: ${totalPrice().toLocaleString('es-CL')}</p>
-      <div className='payment_option'>
+
+      {paymentStep && (
+        <>  
+      <div className='kind_payment'>
         {paymentMethods.map(method => (
         <button
           key={method.id}
           onClick={() => setSelectedPayment(method.id)}
           className={`payment_option ${selectedPayment === method.id ? 'selected' : ''}`}
           >
-            <img className='payment_option img_card'
+            <img className='img_card'
               src={method.img}
               alt={method.label}
               />
+              <p className='payment_title'> {method.label}</p>
           </button>
        ))}
       </div>
-    </div>  
+
+      {(selectedPayment === 'credito' || selectedPayment === 'debito') && (
+        <form className='card_form'>
+          <div>
+            <label>Nombre titular de la Tarjeta</label>
+            <input type='text' placeholder='XXXX XXXX XXXX XXXX' maxLength='19' required />
+          </div>
+          <div className='card_date'>
+            <div>
+              <label> Expiración </label>
+              <input type="text" placeholder='MM/AA' maxLength='5' required />
+            </div>
+            <div>
+              <label> CVV </label>
+              <input type="text" placeholder="XXX" maxLength='3' required />
+            </div>
+          </div> 
+        </form>
+      )}
+      { selectedPayment === 'efectivo' && (
+        <div>
+          <p> El pago en efectivo se realizará al momento de retirar la compra. </p>
+         </div> 
+      )}
+      </>
+      )}
+     </div> 
+     <div className='total_button'>
+      <Button variant='danger' onClick={handleBack} disabled={!paymentStep}>Volver</Button>
+      <Button variant='success' onClick={handlePay} disabled={!paymentStep || !selectedPayment}> Pagar </Button>
+     </div>
     </div>    
-    </main>
+   </main>
   )
  }
 
