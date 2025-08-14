146aadbbd74e8413556c2d36ffa247c66e459f91
Andreina Guerra <141190940+Gealuque@users.noreply.github.com>
2025-08-13 23:36:32 -0400

    Modelo y Controlador

---
+++ backend/src/controllers/cartController.js
@@ -0,0 +1,19 @@
+import { createOrderCartModel, addOrderItemModel } from '../models/cartModel.js'
+
+const postCartOrder = async () => {
+  try {
+    const { userId, items, deliveryType, shippingAddress, recipientName } = req.body
+    const totalAmount = items.reduce((sum, item) => sum + item.unit_price * item.quantity, 0)
+    const newOrder = await createOrderCartModel(userId, 1, deliveryType, shippingAddress, recipientName, totalAmount)
+
+    for (const item of items) {
+      await addOrderItemModel(newOrder.id, item.product_id, item.quantity, item.unit_price)
+    }
+    res.status(201).json({ message: 'Carrito Generado', orderId: newOrder.id })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: 'Error al guardar el carrito' })
+  }
+}
+
+export default postCartOrder
