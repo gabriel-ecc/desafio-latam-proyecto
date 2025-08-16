a9c0e28beab2d58c55664fab475e766a9558bcf6
Andreina Guerra <141190940+Gealuque@users.noreply.github.com>
2025-08-15 21:56:43 -0400

    Limpieza de código

--- backend/src/controllers/ordersController.js
+++ backend/src/controllers/ordersController.js
@@ -2,18 +2,26 @@ import { createOrderCartModel, addOrderItemModel } from '../models/ordersModel.j
 
 const postCartOrder = async (req, res) => {
   try {
-    const { userId = req.body.user_id, items, deliveryType, shippingAddress, recipientName } = req.body
+    const {
+      user_id,
+      order_status,
+      delivery_type,
+      shipping_address,
+      recipient_name,
+      items
+    } = req.body
 
     console.log('Payload recibido:', req.body)
+
     const totalAmount = items.reduce((sum, item) => sum + item.unit_price * item.quantity, 0)
-    const payload = req.body
+
     const newOrder = await createOrderCartModel({
-      userId: payload.user_id,
-      orderStatus: payload.order_status,
-      deliveryType: payload.delivery_type,
-      shippingAddress: payload.shipping_address,
-      recipientName: payload.recipient_name,
-      totalAmount: payload.total_amount
+      userId: user_id,
+      orderStatus: order_status,
+      deliveryType: delivery_type,
+      shippingAddress: shipping_address,
+      recipientName: recipient_name,
+      totalAmount
     })
 
     for (const item of items) {
