a4bc13bf3922dab7d9c70f452daa05d4277881bd
Oscar Garin <oscargarin@gmail.com>
2025-08-19 20:33:36 -0400

    configuracion supabase

--- backend/db/schema/config.js
+++ backend/db/schema/config.js
@@ -1,33 +1,23 @@
 import pg from 'pg'
-process.loadEnvFile()
+import 'dotenv/config'
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } = process.env
+const { Pool } = pg
 
-/*
-console.log('DEBUG: DB_HOST: ', DB_HOST)
-console.log('DEBUG: DB_USER: ', DB_USER)
-console.log('DEBUG: DB_PASSWORD: ', DB_PASSWORD)
-console.log('DEBUG: DB_DATABASE: ', DB_DATABASE)
-console.log('DEBUG: DB_PORT: ', DB_PORT)
-*/
+const { DATABASE_URL } = process.env
 
-const pool = new pg.Pool({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_DATABASE,
-  port: DB_PORT,
-  allowExitOnIdle: true,
-  // Configuración para UTF-8
-  client_encoding: 'UTF8'
-})
+const config = {
+  connectionString: DATABASE_URL,
+  allowExitOnIdle: true
+}
 
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.log('Error connecting to DB', err)
-  } else {
+const pool = new Pool(config)
+
+pool.query('SELECT NOW()')
+  .then(res => {
     console.log('DB Connected', res.rows[0])
-  }
-})
+  })
+  .catch(err => {
+    console.log('Error connecting to DB', err)
+  })
 
 export default pool
